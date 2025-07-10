8d00f996fc433b1f699fc858b541d8f9ee4f8f9a
rey8bit <133556984+rey8bit@users.noreply.github.com>
2025-07-11 03:27:57 +0800

    Update script.js

--- url_image/website/script.js
+++ url_image/website/script.js
@@ -1,1 +1,7 @@
-
+fetch('data/billboard_data.json') // Sesuaikan path-nya
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Data JSON akan ada di sini
+        // Lakukan sesuatu dengan data, misalnya update tampilan
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
