ed15dbfd159da25e298df52fc92ff2920d6fad51
rey8bit <133556984+rey8bit@users.noreply.github.com>
2025-07-13 08:46:07 +0800

    Delete url_image/website/script.js

--- url_image/website/script.js
+++
@@ -1,7 +0,0 @@
-fetch('data/billboard_data.json') // Sesuaikan path-nya
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Data JSON akan ada di sini
-        // Lakukan sesuatu dengan data, misalnya update tampilan
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
